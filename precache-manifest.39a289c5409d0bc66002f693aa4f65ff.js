8cfc7017b218e7b5b2d463c91b2d2cff20f0a8bd
atharvasharma <atharvasjp1@gmail.com>
2020-06-06 18:44:36 +0530

    Updates

---
+++ precache-manifest.39a289c5409d0bc66002f693aa4f65ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733e8cb5972ef68529d5165a657084cd",
+    "url": "/Registration-Management/index.html"
+  },
+  {
+    "revision": "d3c810d61bad6af86210",
+    "url": "/Registration-Management/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f9e64594b3311d758936",
+    "url": "/Registration-Management/static/js/2.44803fec.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Registration-Management/static/js/2.44803fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c810d61bad6af86210",
+    "url": "/Registration-Management/static/js/main.562eb098.chunk.js"
+  },
+  {
+    "revision": "d47ec29c8922635afd1d",
+    "url": "/Registration-Management/static/js/runtime-main.d581f1d3.js"
+  }
+]);
